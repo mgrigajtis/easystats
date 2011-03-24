3b69b3f9677f15820f1c92032110a32cd85e8da8
JustinCampbell <cmsjustin@gmail.com>
2011-03-24 15:00:28 -0400

    Autotest needs to know where the tests are

--- test/easystats_test.rb
+++ test/easystats_test.rb
@@ -1,5 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/test_helper')
 
+# TODO: autotest "Unable to map class EasystatsTest to a file"
 class EasystatsTest < Test::Unit::TestCase
   
   def test_do_the_tests_work
